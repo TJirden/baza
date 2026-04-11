beede854250ed188ff003586dac7e5485974dc91
TJirden <badert.06.amir@gmail.com>
2026-04-11 20:14:57 +0300

    Рефакторинг

--- src/main/java/cringe/baza/presentation/dto/MemeRequest.java
+++
@@ -1,7 +0,0 @@
-package cringe.baza.presentation.dto;
-
-import com.fasterxml.jackson.annotation.JsonProperty;
-
-public record MemeRequest(
-        @JsonProperty("description") String description
-) {}
